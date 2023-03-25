f737cb8519e9f78ec56dca095235cdc868a9280e
Luis Felipe <44214554+Felipeysz@users.noreply.github.com>
2023-03-24 21:19:40 -0300

    Add files via upload

---
+++ Projeto PI BDD/PHP/loginsair.php
@@ -0,0 +1,12 @@
+<?php
+
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'Modelagem_DenysV4';
+
+  $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+?>
