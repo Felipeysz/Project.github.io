e60970e323f86afd59a039ce0624badcd58ec103
Luis Felipe <44214554+Felipeysz@users.noreply.github.com>
2023-04-22 13:20:59 -0300

    Delete login directory

--- login/PI BDA/php style/sair.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['email']);
-    unset($_SESSION['senha']);
-    header("Location: loginbolinha.php");
-    ?>
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
--- login/PI BDA/projeto_login/administrativo.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!empty($_SESSION['idlogin'])){
-	echo "Olá ".$_SESSION['user'].", Bem vindo ao Universo Startup <br>";
-	echo "<a href='sair.php'>Sair</a>";
-}else{
-	$_SESSION['msg'] = "Área restrita";
-	header("Location: login.php");	
-}
